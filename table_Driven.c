0dfdd61bebe872ee3bac9676d8bdf34b6ea81ebc
Manan Hora <horamanan@gmail.com>
2016-10-18 15:57:49 -0400

    changed match to matchTD
    - because we had 2 different ‘match’ functions in the same project

--- table_Driven.c
+++ table_Driven.c
@@ -58,7 +58,7 @@ list* currentToken;
 stack* currentStack;
 
 
-TREE match(token expected) {
+TREE matchTD(token expected) {
 
     if (expected == currentToken->type) {
 
@@ -163,7 +163,7 @@ void ProgramTD(list* tokens) {
 
             if (currentStack->type == currentToken->type) {
 
-                TREE leaf = match(currentStack->type);
+                TREE leaf = matchTD(currentStack->type);
 
             }
         }
